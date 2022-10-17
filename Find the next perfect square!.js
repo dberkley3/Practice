083c7a4edadb6ef11eafb3778867599aca6c7cda
David <dberkley3@gmail.com>
2022-10-17 10:29:19 -0400

    Add files via upload

---
+++ Find the next perfect square!.js
@@ -0,0 +1,10 @@
+// Find the next perfect square!
+
+function findNextSquare(sq) {
+    if (Number.isInteger(Math.sqrt(sq))) 
+  
+      return (Math.sqrt(sq)+1)*(Math.sqrt(sq)+1); 
+  
+    else 
+    return -1;
+  }
